9227df78d715717705c67ba9d179ffe15e9c3059
SwapnilKumbhar7 <156442590+SwapnilKumbhar7@users.noreply.github.com>
2024-11-19 23:52:32 +0530

    Create Bubble_sort.java

---
+++ Bubble_sort.java
@@ -0,0 +1,15 @@
+class Bubble_sort {
+    // Function to sort the array using bubble sort algorithm.
+    public static void bubbleSort(int arr[]) {
+        // code here
+        for(int i=0;i<arr.length;i++){
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+            }
+        }
+    }
+}
